dddf0e52f011104698f4ab6b68b734ba9eb19a75
Kristyan Yochev <kris.yochev@gmail.com>
2021-11-13 17:58:21 +0000

    Restarting server from scratch

--- server-side/src/main.rs
+++ server-side/src/main.rs
@@ -1,24 +1,3 @@
-pub mod player;
-pub mod game;
-
-pub mod prelude {
-    pub use crate::player::*;
-    pub use crate::game::*;
-}
-
-use std::net::TcpListener;
-use crate::prelude::*;
-
 fn main() {
-    let game = Game::new();
-    let (game_thread, game_tx) = game.run();
-
-    let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        game_tx.send(GameMessage::PlayerJoin(stream)).unwrap();
-    }
-    game_thread.join().unwrap();
+    println!("Hello, World!");
 }
