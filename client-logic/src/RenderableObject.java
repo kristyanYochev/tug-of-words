3f0a957f0634292896b826f6b69961c8cff9285c
NathanIsBored13 <54941652+NathanIsBored13@users.noreply.github.com>
2021-11-14 02:24:03 +0000

    die

--- client-logic/src/RenderableObject.java
+++
@@ -1,3 +0,0 @@
-public enum RenderableObject {
-  Test_Button, JoinButton, Textbox;
-}
--- client-logic/src/Textbox.java
+++ client-logic/src/Textbox.java
@@ -1,15 +1,15 @@
-
+import java.util.HashMap;
 
 public abstract class Textbox extends Renderable {
   protected String input; // stores the input of the text box
 
   public Textbox() {
-    super();
+    super(new HashMap<>());
   }
 
   @Override
   public RenderableObject getType() {
-    return RenderableObject.Textbox;
+    return "text_box";
   }
 
   @Override
